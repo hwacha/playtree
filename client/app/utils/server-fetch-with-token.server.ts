635ca5d357e1ceaad87820eeabfadafa2bc96812
Bill Marcy <wgmarcy@gmail.com>
2025-03-05 12:28:43 -0700

    Add debug print statements, fix error response from fetchWithServer

--- client/app/utils/server-fetch-with-token.server.ts
+++ client/app/utils/server-fetch-with-token.server.ts
@@ -3,6 +3,7 @@ import { commitSession, getSession } from "../sessions"
 import { REMIX_SERVER_API_PATH } from "../api_endpoints"
 
 export const serverFetchWithToken = async (request: Request, ...args: Parameters<typeof fetch>): ReturnType<typeof fetch> => {
+	console.log("server fetch with token", request)
 	let options: any = args[1]
 	if (!options) {
 		options = {}
@@ -18,30 +19,35 @@ export const serverFetchWithToken = async (request: Request, ...args: Parameters
 	if (!accessToken) {
 		// if the access token does not exist but the refresh token does, hit the refresh endpoint
 		if (refreshToken) {
+			console.log("fetching token refresh endpoint")
 			const refreshTokenResponse = await fetch(REMIX_SERVER_API_PATH + "/refresh-token", { method: "POST" })
 			if (!refreshTokenResponse.ok) {
-				return new Response("Could not get access token with refresh token", { status: 401 })
+				return new Response(JSON.stringify({ error: "Could not get access token with refresh token"}), { status: 401 })
 			}
 
+			console.log("reading refresh body")
 			const refreshTokenResponseBody = await refreshTokenResponse.json()
 			accessToken = refreshTokenResponseBody.access_token
 		} else {
-			return new Response("Do not have access token or refresh token", { status: 401 })
+			return new Response(JSON.stringify({error: "Do not have access token or refresh token"}), { status: 401 })
 		}
 	}
 
 	options.headers.Authorization = "Bearer " + accessToken;
 	args[1] = options
+	console.log("fetching initial fetch")
 	const initialFetch = await fetch(...args)
-
 	if (initialFetch.ok) {
+		console.log("returning new response with response body and set cookies")
 		return new Response(initialFetch.body, {
 			headers: { ...initialFetch.headers, "Set-Cookie": await commitSession(session) }
 		})
 	} else if (initialFetch.status === 401) { // this indicates reauthentication is worth trying
+		console.log("fetching token refresh endpoint after initial fetch failed with 401")
 		const refreshTokenResponse = await fetch(REMIX_SERVER_API_PATH + "/refresh-token", { method: "POST" })
 		if (!refreshTokenResponse.ok) {
-			return new Response("Access token expired, and could not get new access token with refresh token", { status: 401 })
+			console.log("returning new error response")
+			return new Response(JSON.stringify({ error: "Access token expired, and could not get new access token with refresh token" }), { status: 401 })
 		}
 
 		const refreshTokenResponseBody = await refreshTokenResponse.json()
@@ -53,8 +59,10 @@ export const serverFetchWithToken = async (request: Request, ...args: Parameters
 			(args[1].headers as any)["Set-Cookie"] = await commitSession(session)
 		}
 
+		console.log("making second fetch with ")
 		return fetch(...args)
 	} else {
+		console.log("returning initial fetch which is an error response without a cookie")
 		return initialFetch
 	}
 }
